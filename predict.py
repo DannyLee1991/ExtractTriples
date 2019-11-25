4064b25b1f7db72024abd22d50ef8e78c9db1bd6
lijianan <lijianan@tezign.com>
2019-11-25 15:24:24 +0800

    fix

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
-from kg_with_ds import predict
+from model import train_model
+from config import model_path
 
 if __name__ == '__main__':
+    train_model.load_weights(model_path)
     sentence_list = [
         '如何演好自己的角色，请读《演员自我修养》《喜剧之王》周星驰崛起于穷困潦倒之中的独门秘笈',
         '《下半生留住你一直相爱》是商人醉猫写的网络小说连载于17k小说网',
@@ -10,15 +12,15 @@ if __name__ == '__main__':
         '《狙击部队》是由江苏海润影视制作有限公司出品的电视剧，由赵浚凯执导，王珂、赵聪、刘鑫等主演',
         '查尔斯·阿兰基斯（Charles Aránguiz），1989年4月17日出生于智利圣地亚哥，智利职业足球运动员，司职中场，效力于德国足球甲级联赛勒沃库森足球俱乐部',
         '马志舟，1907年出生，陕西三原人，汉族，中国共产党，任红四团第一连连长，1933年逝世',
-        '斑刺莺是雀形目、剌嘴莺科的一种动物，分布于澳大利亚和新西w兰，包括澳大利亚、新西兰、塔斯马尼亚及其附近的岛屿',
+        '斑刺莺是雀形目、剌嘴莺科的一种动物，分布于澳大利亚和新西兰，包括澳大利亚、新西兰、塔斯马尼亚及其附近的岛屿',
         '伴随着春节的结束，又有一批新剧播出，包括张一山的《我的父亲我的兵》，左小青的《台湾往事》，还有《风光大嫁》等'
     ]
     for item in predict(sentence_list=sentence_list):
-        sent, spo_list = item
         print("-----------")
+        sent, result_list = item
         print("原句: {sent}".format(sent=sent))
         print("三元组:")
-        for item in spo_list:
+        for item in result_list:
             s = item['subject']
             p = item['predicate']
             o = item['object']
--- train.py
+++ train.py
@@ -1,4 +1,8 @@
-from kg_with_ds import train
+from model import train_model, train_D, evaluator
 
 if __name__ == '__main__':
-    train()
+    train_model.fit_generator(train_D.__iter__(),
+                              steps_per_epoch=len(train_D),
+                              epochs=120,
+                              callbacks=[evaluator]
+                              )
