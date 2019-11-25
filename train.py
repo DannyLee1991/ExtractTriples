039b36b8932eb5543a5363d5f1d51c4a9e941bd2
lijianan <lijianan@tezign.com>
2019-11-25 15:35:21 +0800

    rollback

--- train.py
+++ train.py
@@ -1,8 +1,4 @@
-from model import train_model, train_D, evaluator
+from kg_with_ds import train
 
 if __name__ == '__main__':
-    train_model.fit_generator(train_D.__iter__(),
-                              steps_per_epoch=len(train_D),
-                              epochs=120,
-                              callbacks=[evaluator]
-                              )
+    train()
